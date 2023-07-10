399cb14e43bb9cc5e29000769201766c52fa1b8f
Andriey Ronwyll Nacion <acnacion9.8@gmail.com>
2023-07-10 20:47:10 +0800

    done

--- application/views/dashboard/class/email_validate.php
+++
@@ -1,6 +0,0 @@
-<script>
-function isEmail(email) {
-  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-  return regex.test(email);
-}
-</script>
--- application/views/dashboard/class/input-text-accept-money-only.php
+++
@@ -1,9 +0,0 @@
-<script>
-    function accept_money_only(id_textfield){
-        let id = "#"+id_textfield;
-        $(id).on('input',function(){
-            this.value = this.value.replace(/[^0-9.]/g,'');
-            this.value = this.value.replace(/(\..*)\./g, '');
-        })
-    }
-</script>
